292974e00c5cb374ade7b5f8814c39cf1d2a110d
Mercury 1089 <1089software@gmail.com>
2025-01-28 17:30:55 -0500

    fixed 'sqaure'  to 'square'

--- src/main/java/frc/robot/util/MercMath.java
+++ src/main/java/frc/robot/util/MercMath.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 public class MercMath {
     public static Supplier<Double> zeroSupplier = () -> 0.0;
 
-    public static double sqaureInput(double input) {
+    public static double squareInput(double input) {
         return input > 0.0 ? Math.pow(input, 2) : -Math.pow(input, 2);
     }
 }
